96322de63baf640d689494c944b73107a692ac79
HANSANGJUN <hsj11962@naver.com>
2022-08-04 03:17:14 +0900

    SearchPage,modal close 기능추가

--- react-netfilx-clone/src/components/Nav.js
+++ react-netfilx-clone/src/components/Nav.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 export default function Nav() {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -17,6 +20,11 @@ export default function Nav() {
     };
   }, []);
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+    // console.log(e);
+  };
   return (
     <nav className={`nav ${show && "nav_black"} `}>
       <img
@@ -24,7 +32,16 @@ export default function Nav() {
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         // src="https://logos.flamingtext.com/City-Logos/Han-Amped-Logo.png"
         className="nav_logo"
-        onClick={() => window.location.reload()}
+        // onClick={() => window.location.reload()}
+        onClick={() => navigate(`/`)}
+      />
+
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="검색할 영화를 입력하세요"
       />
 
       <img
