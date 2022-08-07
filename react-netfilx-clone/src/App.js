52aefc53d3fdd6604ce88240493068bad53ad77c
HANSANGJUN <hsj11962@naver.com>
2022-08-07 20:49:42 +0900

    feat swiper

--- react-netfilx-clone/src/App.js
+++ react-netfilx-clone/src/App.js
@@ -27,7 +27,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<MainPage />} />
-          <Route path=":movieId" element={<DetailPage />} />
+          <Route path=":MovieId" element={<DetailPage />} />
           <Route path="search" element={<SearchPage />} />
         </Route>
       </Routes>
