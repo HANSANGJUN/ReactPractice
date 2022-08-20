aecd2450976c72c1b53c51d320cd51c10394e430
HANSANGJUN <hsj11962@naver.com>
2022-08-20 20:31:29 +0900

    feat json read,delete

---
+++ react-voca/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+import Day from "./component/Day";
+import DayList from "./component/DayList";
+import Header from "./component/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route path="*" element={<EmptyPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
